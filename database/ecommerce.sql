4a1210a032a4f16bcd0821181d600fb6036031fa
Gaare22 <143631454+Gaare22@users.noreply.github.com>
2024-01-08 01:39:21 +0700

    Add files via upload

---
+++ database/ecommerce.sql
@@ -0,0 +1,190 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 07 Jan 2024 pada 19.38
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecommerce`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `product`
+--
+
+CREATE TABLE `product` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(120) NOT NULL,
+  `keterangan` varchar(225) NOT NULL,
+  `kategori` varchar(60) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `stok` int(4) NOT NULL,
+  `gambar` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `product`
+--
+
+INSERT INTO `product` (`id_barang`, `nama_barang`, `keterangan`, `kategori`, `harga`, `stok`, `gambar`) VALUES
+(1, 'Sepatu', 'Sepatu Merk Converse Tipe All Star', 'Pakaian Pria', 800000, 7, 'Converse.jpg'),
+(2, 'Gaun Pesta', 'dari Brand Ternama Doir', 'Pakaian Wanita', 3000000, 5, 'Gaun.jpg'),
+(4, 'Kamera', 'Canon Tipe EOS 700D', 'Elektronik', 4000000, 2, 'Kamera.jpg'),
+(5, 'Bola Voli', 'Merk Mikasa', 'Peralatan Olahraga', 75000, 30, 'bola_voli.jpg'),
+(6, 'Bola Basket', 'Merk Molten', 'Peralatan Olahraga', 150000, 30, 'Bola Basket.jpg'),
+(7, 'Bola Futsal', 'Merk Specs', 'Peralatan Olahraga', 125000, 17, 'bola_futsal1.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_invoice`
+--
+
+CREATE TABLE `tb_invoice` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(56) NOT NULL,
+  `alamat` varchar(225) NOT NULL,
+  `tanggal_pesan` datetime NOT NULL,
+  `batas_bayar` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_invoice`
+--
+
+INSERT INTO `tb_invoice` (`id`, `nama`, `alamat`, `tanggal_pesan`, `batas_bayar`) VALUES
+(1, 'Tegar', 'Jakarta', '2024-01-03 00:21:34', '2024-01-04 00:21:34');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_pesanan`
+--
+
+CREATE TABLE `tb_pesanan` (
+  `id` int(11) NOT NULL,
+  `id_invoice` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `jumlah` int(3) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `pilihan` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_pesanan`
+--
+
+INSERT INTO `tb_pesanan` (`id`, `id_invoice`, `id_barang`, `nama_barang`, `jumlah`, `harga`, `pilihan`) VALUES
+(1, 1, 1, 'Sepatu', 1, 800000, ''),
+(2, 1, 4, 'Kamera', 1, 4000000, ''),
+(3, 1, 6, 'Bola Basket', 1, 125000, '');
+
+--
+-- Trigger `tb_pesanan`
+--
+DELIMITER $$
+CREATE TRIGGER `pesanan_penjualan` AFTER INSERT ON `tb_pesanan` FOR EACH ROW BEGIN
+	UPDATE product SET stok = stok - NEW.jumlah
+    WHERE id_barang = NEW.id_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_user`
+--
+
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role_id` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tb_user`
+--
+
+INSERT INTO `tb_user` (`id`, `nama`, `username`, `password`, `role_id`) VALUES
+(1, 'admin', 'admin', 'admin123', 1),
+(2, 'Tegar', 'Tegar', '123', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `product`
+--
+ALTER TABLE `product`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_invoice`
+--
+ALTER TABLE `tb_invoice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_pesanan`
+--
+ALTER TABLE `tb_pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_user`
+--
+ALTER TABLE `tb_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
